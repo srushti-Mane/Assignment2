564bf2120644be4ee36d9f4aea421e0e53267a97
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-15 18:24:20 +0530

    modified:code for equality calculation feature

--- linecomputation.java
+++ linecomputation.java
@@ -12,7 +12,21 @@
             int x2 = scanner.nextInt();
             int y2 = scanner.nextInt();
             System.out.println("the values of (x2,y2) ;" + x2 + "," + y2);
-            double length = Math.sqrt(Math.pow(x2 - x1,2) +  Math.pow(y2 - y1, 2));
-            System.out.println("length of line :" + length);
+            double length1 = Math.sqrt(Math.pow(x2 - x1,2) +  Math.pow(y2 - y1, 2));
+            System.out.println("length of line :" + length1);
+            System.out.println("enter x3 and y3");
+            int x3 = scanner.nextInt();
+            int y3 = scanner.nextInt();
+            System.out.println("the values of (x3,y3) ;" + x3 + "," + y3);
+            System.out.println("enter x4 and y4");
+            int x4 = scanner.nextInt();
+            int y4 = scanner.nextInt();
+            System.out.println("the values of (x4,y4) ;" + x4 + "," + y4);
+            double length2 = Math.sqrt(Math.pow(x4 - x3,2) +  Math.pow(y4 - y3, 2));
+            System.out.println("length of line :" + length2);
+
+
+
+
         }
     }
